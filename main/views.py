a8fc6e35fc29299012410d950c08c6dc31a63897
Beloff Lab <belofflab@gmail.com>
2023-08-03 10:56:16 +0300

    update first page

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "main/homepage.html", context={})
+    return render(request, "main/index.html", context={})
 
 
 def about(request):
