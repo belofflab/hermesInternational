39bffc1d07c37c014fa7746a5248afc1a82d04c4
Beloff Lab <belofflab@gmail.com>
2023-08-30 10:01:29 +0300

    update

--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
+from django.core.mail import send_mail
+
 
 def index(request):
     return render(request, "main/index.html", context={})
 
 def shops(request):
     return render(request, "main/shops.html", context={})
+
+def email_test(request):
+    send_mail("123", "123", "Hermes International <support@hermesinternational.ru>", recipient_list=["belofflab@gmail.com"])
