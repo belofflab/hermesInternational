367b46c8daa5ab573b9d9b59f43ef43ce27897a1
Beloff Lab <belofflab@gmail.com>
2023-09-10 12:34:05 +0300

    1

--- main/signals.py
+++ main/signals.py
@@ -1,5 +1,10 @@
-from django.db.models.signals import post_save
+import re
+
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+
+from accounts.models import Purchase
 
 from .models import AccountWarehouse, Warehouse, WarehouseShop
 
@@ -16,6 +21,7 @@ shops = [
     # {"name": "Nordstorm", "image": "img/shops/nordstorm.svg"},
 ]
 
+
 @receiver(post_save, sender=Warehouse)
 def post_save_warehouse(created, instance, **kwargs):
     if created:
@@ -28,3 +34,27 @@ def post_save_account_warehouse(created, instance, **kwargs):
     if created:
         for shop in shops:
             WarehouseShop.objects.create(warehouse=instance, **shop)
+
+
+@receiver(post_delete, sender=Purchase)
+def update_warehouse_shop_quantity(sender, instance, **kwargs):
+    def update_warehouse_quantity(url: str, shops):
+        match = re.search(r"(www\.)?(\w+\.\w+)", url)
+        matched_shop = match.group(2)
+
+        for shop in shops:
+            if matched_shop.startswith(shop.name):
+                shop.quantity = shop.quantity - 1
+                shop.save()
+
+    if instance.delivery_warehouse:
+        try:
+            wss = WarehouseShop.objects.filter(
+                content_type=ContentType.objects.get_for_model(
+                    instance.delivery_warehouse
+                ),
+                object_id=instance.delivery_warehouse.id,
+            ).all()
+            update_warehouse_quantity(url=instance.link, shops=wss)
+        except WarehouseShop.DoesNotExist:
+            print("Not Found model WarehouseShop")
